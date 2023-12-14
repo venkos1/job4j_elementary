72113af63b02e703e62d07c2d9834d55f94fb342
venkos <vvvenkos@gmail.com>
2023-12-14 19:49:01 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/Checkstyle/Broken.java
@@ -0,0 +1,19 @@
+package ru.job4j.Checkstyle;
+
+public class Broken {
+    private int size_of_empty = 10;
+    public String surname;
+    public static final String newValue = "";
+
+    void echo() { }
+
+    void Media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+        String name;
+        Broken() { }
+    }
+
+    void method (int a, int b, int c, int d, int e) { }
+}
